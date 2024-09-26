a749028c0ff0aa077c25ab00406a2254ccf2ebe0
Cho Yeonsoo <meriel1010@hanyang.ac.kr>
2024-09-26 14:47:54 +0900

    feature/습관공유 커뮤니티 layout, filter 구현

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div>Header 영역</div>
+</template>
+
+<style scoped>
+div {
+    background-color: black;
+    color: white;
+    height: 250px;
+    text-align: center;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import HomePage2 from '../pages/HomePage2.vue';
+import RoutineCommunity from '../pages/RoutineCommunity.vue';
+import Card from '../pages/card.vue';
+import Card2 from '../pages/card2.vue';
 
 
 const router = createRouter({
@@ -15,6 +18,21 @@ const router = createRouter({
         path: '/home2',
         name: 'home2', 
         component: HomePage2 },
+    {
+        path: '/routine_community',
+        name: 'routineCommunity',
+        component: RoutineCommunity 
+    },
+    {
+      path: '/test',
+      name: 'card',
+      component: Card 
+    },
+    {
+      path: '/test2',
+      name: 'card2',
+      component: Card2
+  }
   ],
 });
 
