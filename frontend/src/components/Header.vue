5123095e24432fe82812ecec666b571a1cd96b2f
ZeroKgun <[Github>
2024-09-26 11:43:55 +0900

    refactor:header community에 공유, 인증 2개 추가

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -26,7 +26,8 @@
                 Community
                 </button>
                 <ul class="dropdown-menu menu-item" aria-labelledby="dropdownMenuButton1">
-                <li><a class="dropdown-item" href="home">커뮤니티</a></li>
+                <li><a class="dropdown-item" href="home">습관 공유 커뮤니티</a></li>
+                <li><a class="dropdown-item" href="home">인증 커뮤니티</a></li>
                 </ul>
             </div>
         </div>
