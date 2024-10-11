5ad2aa6d9c10086810f5a75a1c6a8601d566acb9
jsh9611 <jangsh9611@naver.com>
2024-10-11 21:08:31 +0900

    fix: the problem where the fade is brought up two times

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 // import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
