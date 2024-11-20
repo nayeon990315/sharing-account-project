4ea2386806acc73095765411625f0f375f3c7bed
99skdus@naver.com <99skdus@naver.com>
2024-11-20 16:12:27 +0900

    Update RoomBasic

--- src/main/java/com/example/backend/roombasic/mapper/RoomBasicMapper.java
+++ src/main/java/com/example/backend/roombasic/mapper/RoomBasicMapper.java
@@ -8,11 +8,12 @@ import java.util.Map;
 @Mapper
 public interface RoomBasicMapper {
     // 1. 방 개설 (새로운 방 개설하면 상세 내용 추가) - RoomList
-    public void insertRoom(RoomBasicVO roomBasicVO);
+    int insertRoom(RoomBasicVO roomBasicVO);
 
     // 2. 사용자 추가 (새로운 방 개설하면 개설한 사람의 역할이 방장으로 설정되어 추가) - PersonList & RoomComplete
     public void insertTeamLeader(Map<String, Object> params);
-    public void insertRoomComplete(Map<String, Object> params);
+    void insertRoomComplete(int roomNum);
+//    public void insertRoomComplete(Map<String, Object> params);
 
     // 3. 방 세부 내용 변경
     public void updateRoomBasic(RoomBasicVO roomBasicVO);
--- src/main/java/com/example/backend/roombasic/vo/RoomBasicVO.java
+++ src/main/java/com/example/backend/roombasic/vo/RoomBasicVO.java
@@ -10,12 +10,28 @@ public class RoomBasicVO {
     private String accountNumber;
     private String title;
     private String category;
-    private int numberTotal;
-    private String period;
-    private Date dueDate;
+    private int memberTotal;
+    private int period;
+    private int dueDate;
     
     // 팀장 ID는 따로 담지 않음
 
     // RoomComplete
-    private int numberCurrent; // 현재 구성원 수 -> 1로 설정(팀장)
+    private int memberCurrent; // 현재 구성원 수 -> 1로 설정(팀장)
+
+    public int getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(int roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public Integer getMemberTotal() {
+        return memberTotal;
+    }
+
+    public void setMemberTotal(Integer memberTotal) {
+        this.memberTotal = memberTotal;
+    }
 }
